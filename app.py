d4ef69e59a910519ac9dd075c97b2579b080cb77
Dave Campbell <dave@daves-air.lan>
2023-06-24 08:56:54 -0400

    a few more lines of code

--- app.py
+++ app.py
@@ -28,4 +28,45 @@ agent= initialize_agent(
 
 # agent("What is the 25% of 300?")
 
-agent("how many episodes were made of the sitcom 'Friends'?")
+# agent("how many episodes were made of the sitcom 'Friends'?")
+
+"""
+question = "Tom M. Mitchell is an American computer scientist \
+and the Founders University Professor at Carnegie Mellon University (CMU)\
+what book did he write?"
+result = agent(question)
+"""
+
+#### Python Agent
+
+agent = create_python_agent(
+    llm,
+    tool=PythonREPLTool(),
+    verbose=True
+)
+
+customer_list = [["Harrison", "Chase"], 
+                 ["Lang", "Chain"],
+                 ["Dolly", "Too"],
+                 ["Elle", "Elem"], 
+                 ["Geoff","Fusion"], 
+                 ["Trance","Former"],
+                 ["Jen","Ayai"]
+                ]
+
+# agent.run(f"""Sort these customers by \
+# last name and then first name \
+# join the first name to the last name without a space and then
+# print each resulting name in reverse and in lower case: {customer_list}""") 
+
+
+# View detailed outputs of the chains
+
+import langchain
+langchain.debug=True
+agent.run(f"""Replace all occurrences of the letter 'e' with \
+the letter 'z' and then sort these customers by \
+last name and then first name \
+and print the output: {customer_list}""") 
+langchain.debug=False
+
